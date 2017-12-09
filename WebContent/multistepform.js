5b0473964f500a3c679e98141db9bbfb620daba5
Randy Petersheim <Randy Petersheim@DESKTOP-BFG2ENH.hsd1.pa.comcast.net>
2017-12-09 16:10:23 -0500

    Added functions to create dummy data and calculate results

--- WebContent/multistepform.js
+++ WebContent/multistepform.js
@@ -15,6 +15,7 @@ $("#next").on('click', function(e) {
 				
 		current_fs.hide();
 		next_fs.show();
+		$('#results-table').show();
 	}
 	
 });
@@ -25,11 +26,79 @@ $("#measure-sample").on('click', function(e) {
 		var measurementObj = {};
 		
 		measurementObj.sampleName = $('#sample-name').val();
-		measurementObj.date = new Date();
-		measurementObj.measurementArray = [3454, 3567, 3673];
-		measurementObj.mean = 3452;
-		measurementObj.stdDev = 45;
+		measurementObj.date = (new Date()).toLocaleString('en-US');
+		measurementObj.measurementArray = createDummyData(newProject.methodName);
+		measurementObj.mean = calculateMean(measurementObj.measurementArray);
+		measurementObj.stdDev = calculateStdDev(measurementObj.measurementArray);
+		measurementObj.coatingWeight = calculateCoatingWeight(newProject.methodName, measurementObj.mean);
 		
 		newProject.measurements.push(measurementObj);
+		pushResultsToTable(measurementObj);
+		
+		$('#sample-name').val('');
 	}
 });
+
+function createDummyData(method) {
+	var dataArray = [];
+	var min = this[method].min;
+	var max = this[method].max;
+	var factor = this[method].factor;
+	var baseRandom = randBetween(min, max);
+	
+	for(var i = 0; i < 3; i++) {
+		dataArray[i] = randBetween(baseRandom - 100, baseRandom + 100);
+	}
+	
+	return dataArray;
+}
+
+function randBetween(min, max) {
+	return Math.round(Math.random() * (max - min + 1)) + min;
+}
+
+function calculateMean(dataArray) {
+	var mean = 3;
+	
+	for(var i = 0; i < dataArray.length; i++) {
+		mean += dataArray[i];
+	}
+	
+	mean = Math.round(mean /dataArray.length);
+	
+	return mean;
+}
+
+function calculateStdDev(dataArray) {
+	var stdDev;
+	var stdPrep = 0;
+	var mean = calculateMean(dataArray);
+	
+	for(var i = 0; i < dataArray.length; i++) {
+		stdPrep += Math.pow(dataArray[i] - mean, 2);
+	}
+	
+	stdDev = Math.round(Math.sqrt(stdPrep/dataArray.length));
+	
+	return stdDev;
+}
+
+function calculateCoatingWeight(method, mean) {
+	var factor = this[method].factor;
+	return Math.round(factor * mean);	
+}
+
+function pushResultsToTable(measurementObj) {
+	var $row = $('<tr></tr>');
+	
+	$row.append($('<td></td>').text(measurementObj.sampleName));
+	$row.append($('<td></td>').text(measurementObj.date));
+	$row.append($('<td></td>').text(measurementObj.measurementArray[0]));
+	$row.append($('<td></td>').text(measurementObj.measurementArray[1]));
+	$row.append($('<td></td>').text(measurementObj.measurementArray[2]));
+	$row.append($('<td></td>').text(measurementObj.mean));
+	$row.append($('<td></td>').text(measurementObj.stdDev));
+	$row.append($('<td></td>').text(measurementObj.coatingWeight));
+	
+	$('tbody').append($row);	
+}
--- WebContent/select.js
+++ WebContent/select.js
@@ -1,24 +1,40 @@
-(function() {
-	var method = document.getElementById('method-name');
-	var substrate = document.getElementById('substrate');
-	var chromePhosphate = {
+var chromePhosphate = {
+		min : 1500,
+		max : 3000,
+		factor : 0.033,
+		substrateList : {
 			alcl: 'Aluminum Coil Stock',
 			alex: 'Aluminum Extrusion'
-	}
-	var chromeOnSteel = {
+		}
+};
+var chromeOnSteel = {
+		min : 500,
+		max : 5000,
+		factor : 0.002,
+		substrateList : {
 			crs: 'CRS',
 			galv: 'Galvanized Steel',
 			glum: 'Galvalume',
 			hrs: 'HRS'
-	}
-	var chromeFree = {
+		}
+};
+var chromeFree = {
+		min : 300,
+		max : 1500,
+		factor : 0.0166,
+		substrateList : {
 			alcl: 'Aluminum Coil Stock',
 			alex: 'Aluminum Extrusion',
 			crs: 'CRS',
 			galv: 'Galvanized Steel',
 			glum: 'Galvalume',
 			hrs: 'HRS'
-	}
+		}
+};
+
+(function() {
+	var method = document.getElementById('method-name');
+	var substrate = document.getElementById('substrate');
 	
 	addEvent(method, 'change', function() {
 		if(this.value === 'choose') {
@@ -36,13 +52,13 @@
 	
 	function getSubstrate(methodName) {
 		if(methodName === 'chromePhosphate') {
-			return chromePhosphate;
+			return chromePhosphate.substrateList;
 		}
 		else if(methodName === 'chromeOnSteel') {
-			return chromeOnSteel;
+			return chromeOnSteel.substrateList;
 		}
 		else if(methodName === 'chromeFree') {
-			return chromeFree;
+			return chromeFree.substrateList;
 		}
 	}
 	
